e589a3ab7ac95e77e6e0c1d3b280ae0f4e83ff2c
Harun Uyguç <harun.uyguc@hotmail.com>
2022-09-29 14:22:16 +0300

    Calculator Update 1

--- Basic Projects/calculator.py
+++ Basic Projects/calculator.py
@@ -1,7 +1,23 @@
-def hesap_makinesi(islemTuru):
-    sayi1 = float(input("1. Sayınızı Giriniz: "))
-    sayi2 = float(input("2. Sayınızı Giriniz: "))
-    
+def hesap_makinesi():
+    islemTurleri = ["toplama", "çıkarma", "çarpma", "bölme"]
+
+    islemTuru = input("Toplama mı, çıkarma mı, çarpma mı, bölme mi yapmak istersiniz?: ")
+    islemTuru = islemTuru.lower()
+
+    while (islemTuru not in islemTurleri):
+        islemTuru = input("Hatalı cevap! Toplama mı, çıkarma mı, çarpma mı, bölme mi yapmak istersiniz?: ")
+        islemTuru = islemTuru.lower()
+
+    try:
+        sayi1 = float(input("1. Sayınızı Giriniz: "))
+        sayi2 = float(input("2. Sayınızı Giriniz: "))
+    except ValueError:
+        sayi1 = float(input("Hatalı giriş! Sayı girmelisiniz! 1. Sayıyı Giriniz: "))
+        sayi2 = float(input("Hatalı giriş! Sayı girmelisiniz! 2. Sayıyı Giriniz: "))
+    except Exception as e:
+        print("Bilinmeyen bir hata oluştu!")
+        print(e)
+
     if (islemTuru == "toplama"):
         sonuc = sayi1 + sayi2
     elif (islemTuru == "çıkarma"):
@@ -22,12 +38,9 @@ def hesap_makinesi(islemTuru):
     yeniBirİslem = input("İşlem yapmaya devam etmek istiyorsan 'devam', istemiyorsan 'çık' yazmalısın!: ")
 
     if (yeniBirİslem == "devam"):
-        islemTuru = input("Toplama mı, çıkarma mı, çarpma mı, bölme mi yapmak istersiniz?: ")
-        hesap_makinesi(islemTuru)
+        hesap_makinesi()
     else:
         print("Hesap makinesi kapatıldı!")
         
 
-islemTuru = input("Toplama mı, çıkarma mı, çarpma mı, bölme mi yapmak istersiniz?: ")
-islemTuru = islemTuru.lower()
-hesap_makinesi(islemTuru)
+hesap_makinesi()
