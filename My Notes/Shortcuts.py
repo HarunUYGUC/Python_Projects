51914d82bb669be5c87018204abf526ad490a897
Harun Uyguç <harun.uyguc@hotmail.com>
2022-10-14 22:19:05 +0300

    Shortcuts

---
+++ My Notes/Shortcuts.py
@@ -0,0 +1,20 @@
+"""
+Önemli bulduğum ve yeni öğrendiğim kısa yollar. :D
+"""
+# Indent Line (Girinti oluşturmak): CTRL + Ü
+# Move Line Up (Satırı yukarıya taşımak): ALT + UpArrow
+# Move Line Down (Satırı aşağıya taşımak): ALT + DownArrow
+
+
+"""
+Önceden bildiğim kısa yollar. 
+"""
+# Copy (Kopyala): CTRL + C
+# Paste (Yapıştır): CTRL + V
+# Cut (Kes): CTRL + X
+
+# Undo (Geri al): CTRL + Z
+# Redo (Geri aldığın şeyi tekrar geri al): CTRL + Y
+
+# Add Line Comment (Toplu şekilde yorum satırı oluşturmak): CTRL + K + C 
+# Remove Line Comment (Toplu şekilde yorum satırını kaldırmak): CTRL + K + U
