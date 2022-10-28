9434de3ecc67d55d6102d21c323d8b3bc3d157b6
Harun Uyguç <harun.uyguc@hotmail.com>
2022-10-28 17:22:49 +0300

    New Shortcuts

--- My Notes/Shortcuts.py
+++ My Notes/Shortcuts.py
@@ -1,12 +1,4 @@
 """
-Önemli bulduğum ve yeni öğrendiğim kısa yollar. :D
-"""
-# Indent Line (Girinti oluşturmak): CTRL + Ü
-# Move Line Up (Satırı yukarıya taşımak): ALT + UpArrow
-# Move Line Down (Satırı aşağıya taşımak): ALT + DownArrow
-
-
-"""
 Önceden bildiğim kısa yollar. 
 """
 # Copy (Kopyala): CTRL + C
@@ -18,3 +10,27 @@
 
 # Add Line Comment (Toplu şekilde yorum satırı oluşturmak): CTRL + K + C 
 # Remove Line Comment (Toplu şekilde yorum satırını kaldırmak): CTRL + K + U
+
+
+"""
+Önemli bulduğum ve yeni öğrendiğim kısa yollar. :D
+"""
+# Indent Line (Girinti oluşturmak): CTRL + Ü
+
+# Move Line Up (Satırı yukarıya taşımak): ALT + UpArrow
+# Move Line Down (Satırı aşağıya taşımak): ALT + DownArrow
+
+# Highlighting Characters (Karakterleri vurgulamak): SHIFT + RightArrow
+# Removing Highlighted Characters (Vurgulanan karakterleri kaldırmak): SHIFT + LeftArrow
+# Highlight Line By Line (Satır satır vurgulamak): CTRL + L
+
+# Move Word By Word (Kelime kelime ilerlemek): CTRL + RightArrow
+# Move Word By Word But In Reverse (Kelime kelime ilerlemek ama geriye doğru): CTRL + LeftArrow
+
+# Select Word and Replace (Kelime seç ve değiştir): CTLR + D (Birden fazla kez bu 
+# kombinasyonu kullanarak aynı kelimeyi diğer kod satırlarından da seçerek bir yerdeki 
+# değişiklik ile aynı anda hepsini değiştirebilirsin.)
+
+# Set Multiple Cursors (Birden çok imleç ayarlamak): ALT + CLICK (Birden fazla kez bu 
+# kombinasyonu kullanarak seçtiğin yerlere ilk tıkladığın yere yazdığın bilgiyi aynı anda
+# hepsine yazmaya yarar.)
